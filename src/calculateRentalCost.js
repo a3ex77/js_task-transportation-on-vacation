40f0bdbfec2a9fd38c82a68315ce48dbf432f0bc
a3ex77 <oleksiy677@gmail.com>
2025-04-23 13:16:00 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,11 +12,13 @@ function calculateRentalCost(days) {
 
   if (days >= longTerm) {
     return oneDayCost * days - discountSevenDays;
-  } else if (days >= shortTerm && days < longTerm) {
-    return oneDayCost * days - discountThreeDays;
-  } else {
-    return oneDayCost * days;
   }
+
+  if (days >= shortTerm && days < longTerm) {
+    return oneDayCost * days - discountThreeDays;
+  }
+
+  return oneDayCost * days;
 }
 
 module.exports = calculateRentalCost;
